be2a08f29d849da43db4240e7a9d6db3489e8ed8
chiefblackhawk <53925183+chiefblackhawk@users.noreply.github.com>
2020-05-16 14:09:34 -0700

    Deposit Commit
    Added functionality to deposit method. thus allowing for user to enter a deposit amount and have text files update with new balance reflecting original balance + deposit.

--- bank_account.py
+++ bank_account.py
@@ -1,10 +1,11 @@
-
+import os
 class BankAccount():
 	#docstring for BankAccount
 	def __init__(self, account_holder):
 		super(BankAccount, self).__init__()
 		self.account_holder = account_holder
 		self.access_granted = False
+		self.file_pin = 0
 
 	def login(self):
 		mega_list = []
@@ -21,6 +22,7 @@ class BankAccount():
 					pin_number = int(input("Please enter your pin number to login: "))
 					if str(pin_number) in sublist:
 						self.access_granted = True
+						self.file_pin = pin_number
 						print("Access Granted")
 						On = False
 					else:
@@ -35,8 +37,6 @@ class BankAccount():
 				else:
 					continue #just continue if currenet account_holder can't be found in current list
 
-
-
 	def balance(self):
 		mega_list = []
 		file = 'access_login.txt'
@@ -44,23 +44,79 @@ class BankAccount():
 		with open(file)as f_obj:
 				for content in f_obj:
 					mega_list.append(content.split())#compile megalist with smaller list created from each line of text file
-		while On:
-			for sublist in mega_list:
-				if self.account_holder in sublist:
-					print(f"Hello {self.account_holder.title()}, your current balance is: ${sublist[-1]}.")
-					On = False
+		#print(f"mega_list: {mega_list}")
+		while self.access_granted:
+			while On:
+				for sublist in mega_list:
+					if self.account_holder in sublist:
+						print(f"Hello {self.account_holder.title()}, your current balance is: ${sublist[-1]}.")
+						On = False
+						self.access_granted = False
 
 	def deposit(self):
+		print("\nIn deposit method")
+		mega_list = []
+		file = 'access_login.txt'
+		active = 'access_login_active.txt'
+		tmp = 'access_login_tmp.txt'
+		On = True
+		with open(file)as f_obj:
+			for content in f_obj:
+				mega_list.append(content.split())#compile megalist with smaller list created from each line of text file
+
+		#print(f'Mega List: {mega_list}')
+		deposit_amount = int(input("How much are you depositing? "))
 		while self.access_granted:
-			deposit_amount = int(input("How much are you depositing? "))
-			balance+=deposit_amount
-			self.access_granted = False
+			while On:
+				for sublist in mega_list:
+					if self.account_holder in sublist:
+						print(f'Sub List: {sublist}')
+						
+						name = self.account_holder
+						dep_pin_number = self.file_pin
+
+						print(f"Hello {self.account_holder.title()}, adding ${deposit_amount} to your balance.")
+						#print(sublist[2])
+						final_deposit_value = int(sublist[2]) + deposit_amount
+						deposit_add = str(final_deposit_value)
+
+						mega_list.remove(sublist) #remove active sublist/megalist index to only focus on adding other indexes to text file records
+						#print(f"Updated Mega List {mega_list}")
+
+						#Write to temp file after converting indexes (sublist) into string seperated by white_space
+						with open(tmp,'w')as f_obj:
+							for line in mega_list:
+								unlist = ' '.join(line) #Using join method to change make sublist/index of megalist mimic a string
+								#print(f"Unlist: {unlist}")
+								f_obj.write(unlist+'\n')
+
+						#Used to read content of temp file as a method of copying content
+						with open(tmp)as f_obj:
+							tpm_content = f_obj.read()
+							#print(f"TPM CONTENT: \n{tpm_content}")
+
+						#Write temp file content to active_file 
+						with open(active,'w')as f_obj:
+							f_obj.write(tpm_content)
+						
+						#Append active name, pin, and new deposit as a method of replacing entire line for user logged in.
+						with open(active,'a') as f_obj:
+							f_obj.write(name+' '+str(dep_pin_number)+' '+str(final_deposit_value))
+							On = False
+						self.access_granted = False
+
+						#copy active to login file to update main file
+						os.system('cp access_login_active.txt access_login.txt')
 
 	def withdraw(self):
 		print('na')
 
 def main():
 	justin = BankAccount('justin')
+	justin.login()
+	justin.balance()
+	justin.login()
+	justin.deposit()
 	justin.login()
 	justin.balance()
 
